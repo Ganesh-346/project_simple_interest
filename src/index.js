4c67a55cebea955041757647878fb6f8e029f978
Ganesh-346 <saiganeshbhimavarapu88@gmail.com>
2023-01-26 15:08:24 +0530

    shh

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Example from './Demor';
+ 
 import reportWebVitals from './reportWebVitals';
-import Home from './Home';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ 
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
-  
-  </React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ 
+// If you want to start measuring performance
+// in your app, pass a function to log results
+// (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint.
 reportWebVitals();
---
+++ src/react_test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+function react_test() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default react_test;
